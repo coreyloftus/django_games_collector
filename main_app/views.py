55faf9745ca2cf4ab58a390d0367b0b4b309e83a
coreyloftus <coreyloftus@gmail.com>
2023-01-25 15:16:36 -0500

    Create Games List route, and game_list.html code

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
 
+from django.views import View
+from django.views.generic.base import TemplateView
 # Create your views here.
+
+from django.http import HttpResponse
+
+
+class Game:
+    def __init__(self, title, genre, publisher, release_date, platform, imageURL):
+        self.title = title
+        self.genre = genre
+        self.publisher = publisher
+        self.release_date = release_date
+        self.platform = platform
+        self.imageURL = imageURL
+
+
+games = [
+    Game(
+        "The Legend of Zelda",
+        "Action",
+        "Nintendo",
+        "February 21, 1986",
+        "NES",
+        "https://www.mobygames.com/images/covers/l/14445-the-legend-of-zelda-nes-front-cover.jpg"
+    ),
+    Game(
+        "Zelda 2: The Adventure of Link",
+        "Action",
+        "Nintendo",
+        "January 17, 1987",
+        "NES",
+        "https://www.mobygames.com/images/covers/l/14437-zelda-ii-the-adventure-of-link-nes-front-cover.jpg"),
+]
+
+class Game_List(TemplateView):
+    template_name= 'game_list.html'
