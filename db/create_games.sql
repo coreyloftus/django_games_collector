4a149e531e4abe61cf53e963e8613bc08fbb191a
coreyloftus <coreyloftus@gmail.com>
2023-01-26 15:03:05 -0500

    db seeded manually -- why did the imageURL property cause an error?

---
+++ db/create_games.sql
@@ -0,0 +1,18 @@
+INSERT INTO main_app_game(title, genre, publisher, release_date, platform, imageURL, favorite)
+VALUES(
+        "The Legend of Zelda",
+        "Action",
+        "Nintendo",
+        "February 21, 1986",
+        "NES",
+        "https://www.mobygames.com/images/covers/l/14445-the-legend-of-zelda-nes-front-cover.jpg", True
+    );
+
+INSERT INTO main_app_game(title, genre, publisher, release_date, platform, imageURL, favorite)
+VALUES (
+        "Zelda 2: The Adventure of Link",
+        "Action",
+        "Nintendo",
+        "January 17, 1987",
+        "NES",
+        "https://www.mobygames.com/images/covers/l/14437-zelda-ii-the-adventure-of-link-nes-front-cover.jpg");
--- main_app/models.py
+++ main_app/models.py
@@ -10,7 +10,7 @@ class Game(models.Model):
     publisher = models.CharField(max_length=100)
     release_date = models.DateField('Date')
     platform = models.CharField(max_length=100)
-    imageURL = models.CharField(max_length=250)
+    imageURL = models.CharField(max_length=500)
     favorite = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
